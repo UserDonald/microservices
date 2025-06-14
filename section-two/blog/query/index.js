74dbab7964f9ffc89e729e1d409ea88b3e734fa2
djnash <djnash@iu.edu>
2025-06-14 16:38:54 -0400

    Add query service with Express for handling posts and events. Included .gitignore for node_modules, and created package.json and package-lock.json for dependency management.

---
+++ section-two/blog/query/index.js
@@ -0,0 +1,37 @@
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import express from 'express';
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const posts = {};
+
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === 'PostCreated') {
+    const { id, content, author, username, createdAt } = data;
+    const newPost = { id, content, author, username, createdAt, comments: [] };
+    posts[id] = newPost;
+  }
+
+  if (type === 'CommentCreated') {
+    const { id, content, postId, author, username, createdAt } = data;
+    const updatedPost = posts[postId];
+    const newComment = { id, content, author, username, createdAt };
+    updatedPost.comments.push(newComment);
+    posts[postId] = updatedPost;
+  }
+
+  res.send({});
+});
+
+app.listen(4002, () => {
+  console.log('Listening on 4002');
+});
